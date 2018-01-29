c8c64fcd1ec4dc2d3ca94f1dc6e9ea85676091e6
Brad Hambrice <hambrice@uab.edu>
2018-01-29 21:56:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if params["username"] == nil
+    if params["username"] == nil || params["password"] == nil
       redirect '/failure'
     else
       @user = User.new(name: params["username"], password: params["password"])
